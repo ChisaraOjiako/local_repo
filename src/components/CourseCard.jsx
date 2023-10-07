2b5131deedfbea2a5b871cbeb715980546f27888
Godfrey Oji <godfreyoji@dhcp-10-105-74-231.wireless.northwestern.private>
2023-10-06 21:26:58 -0500

    course selection

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,8 +1,16 @@
 import './CourseList.css'
+import { useState } from "react";
 
 export default function courseCard({course}){
+    const [selected, setSelected] = useState([]);
+
+    const toggleSelected = (item) => setSelected(
+    selected.includes(item)
+    ? selected.filter(x => x !== item)
+    : [...selected, item]
+  );
     return(
-        <div className='card m-1 p-2'>
+        <div className= {selected.includes(course) ? 'selected-courses': ''} onClick={() => toggleSelected(course)}>
             <div className='card-body'>
                 <h5 className='card-title'> {course.term} CS {course.number} </h5>
                 <p className='card-text'>{course.title}</p>
