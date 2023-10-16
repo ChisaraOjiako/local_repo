366b02335c82ae489637eb000028589a5fa1ca7b
Godfrey Oji <godfreyoji@dhcp-10-105-74-231.wireless.northwestern.private>
2023-10-15 20:46:47 -0500

    course form

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,14 +1,55 @@
-import "./CourseList.css";
+import React, { useState } from 'react';
 
-export default function courseCard({ course }) {
+export default function CourseCard({ course }) {
+  const [editing, setEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(course.title);
+  const [editedMeets, setEditedMeets] = useState(course.meets);
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const onSubmit = () => {
+
+    setEditing(false);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    
+    setEditedTitle(course.title);
+    setEditedMeets(course.meets);
+  };
+
   return (
-      <div className="card-body">
-        <h5 className="card-title">
-          {" "}
-          {course.term} CS {course.number}{" "}
-        </h5>
-        <p className="card-text">{course.title}</p>
-        <p className="card-text">{course.meets}</p>
-      </div>
+    <div className="card-body">
+      {editing ? (
+        <div>
+          <input
+            type="text"
+            placeholder="New title"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="New meets"
+            value={editedMeets}
+            onChange={(e) => setEditedMeets(e.target.value)}
+          />
+          {/* <button onClick={onSubmit}>Submit</button> */}
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <h5 className="card-title">
+            {course.term} CS {course.number}
+          </h5>
+          <p className="card-text">{course.title}</p>
+          <p className="card-text">{course.meets}</p>
+          <button onClick={handleEdit}>Edit</button>
+        </div>
+      )}
+    </div>
   );
 }
